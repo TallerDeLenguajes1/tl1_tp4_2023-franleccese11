9d9b4530ce50d36f46aef7366fe733f557dccbdd
franleccese11 <franchelaleccese@gmail.com>
2023-04-26 19:53:35 -0300

    punto 2 terminado

--- punto2.c
+++ punto2.c
@@ -14,9 +14,13 @@ int Duracion; // entre 10 – 100
 
 void initializeArray(Tarea **task, int n);
 void chargeTask(Tarea **task, int id);
-void showTask(Tarea **task,Tarea **task_completed,int id);
+void showTaskQuest(Tarea **task,Tarea **task_completed,int id);
+void showTask(Tarea **task);
 void showTaskCompleted(Tarea **task_completed, int id);
 void showOutStandingTask(Tarea **task,int id);
+Tarea* searchTaskWord(Tarea **task, Tarea **task_completed,int quantity);
+Tarea* searchTaskId(Tarea **task,Tarea **task_completed,int id,int id_searched);
+void menu(Tarea **task, Tarea **task_completed, int quantity);
 
 
 int main(){
@@ -40,7 +44,7 @@ int main(){
     printf("******Tareas pendientes******\n");
     for (int id = 0; id < quantity; id++)
     {
-        showTask(array_task+id,task_completed+id,id);
+        showTaskQuest(array_task+id,task_completed+id,id);
     }
     puts("*/*/* TAREAS COMPLETADAS */*/*\n");
     for (int id = 0; id < quantity; id++)
@@ -79,7 +83,7 @@ void chargeTask(Tarea **task, int id){
 }
 
 
-void showTask(Tarea **task, Tarea **task_completed, int id){
+void showTaskQuest(Tarea **task, Tarea **task_completed, int id){
     int question;
     printf("Tarea numero:%d\n",id+1);
     printf("descripcion de la tarea:");
@@ -92,6 +96,12 @@ void showTask(Tarea **task, Tarea **task_completed, int id){
         *task_completed = *task;
         *task = NULL;
     }  
+}
+
+void showTask(Tarea **task){
+    printf("descripcion de la tarea:");
+    puts((*task)->Descripcion);
+    printf("la duracion estimada de la tarea es:%d\n",(*task)->Duracion);
 }
 
 
@@ -115,7 +125,7 @@ void showOutStandingTask(Tarea **task,int id){
     } 
 }
 
-Tarea* searchTask(Tarea **task, Tarea **task_completed,int quantity){
+Tarea* searchTaskWord(Tarea **task, Tarea **task_completed,int quantity){
     char *buff;
     char *needle;
     buff = (char*) malloc(sizeof(char)*MAX);
@@ -143,3 +153,97 @@ Tarea* searchTask(Tarea **task, Tarea **task_completed,int quantity){
     (aux)->TareaID = -1;
     return(aux);  
 } 
+
+Tarea* searchTaskId(Tarea **task,Tarea **task_completed,int quantity,int id_searched){
+    for (int i = 0; i < quantity; i++)
+    {
+        if (*(task+i) != NULL)
+        {
+        
+            if((*(task+i))->TareaID == id_searched) { 
+                return(*(task+i));
+            }
+        }
+
+        if (*(task_completed+i) != NULL)
+        {
+            if ((*(task_completed+i))->TareaID == id_searched){
+                return (*(task_completed+i))
+            }
+        }  
+    }
+    Tarea *aux=(Tarea*) malloc(sizeof(Tarea));    
+    (aux)->TareaID = -1;
+    return(aux);   
+}
+
+void menu(Tarea **task, Tarea **task_completed,int quantity){
+    int option;
+    do
+    {
+        puts("<<<<<< MENU >>>>>>\n");
+        puts("seleccione una de las siguientes opciones:\n");
+        puts("<< 0: salir del menu>>\n");
+        puts("<< 1: buscar tarea por ID\n");
+        puts("<< 2: buscar tarea por palabra clave\n");
+        scanf("%d",&option);
+        switch (option)
+        {
+        case 1:
+            int id_searched;
+            puts("**busqueda por ID**\n");
+            puts("ingrese el ID de la tarea que busca:\n");
+            scanf("%d",&id_searched);
+            Tarea* aux = searchTaskId(task,task_completed,quantity,id_searched);
+            break;
+        
+        case 2:
+            puts("**busqueda de la tarea por palabra clave:\n");
+            puts("ingrese la palabra clave:\n");
+            Tarea* aux = searchTaskWord(task,task_completed,quantity);
+            break;
+        default:
+            puts("numero ingresado no valido, vuelva a ingresar un numero valido:\n");
+            scanf("%d",&option);
+            break;    
+        }
+        if (aux->TareaID != -1)
+        {
+            printf("la tarea fue encontrada satisfactoriamente:\n");
+            showTask(aux);
+        }else
+        {
+            printf("no se encontro la tarea");
+        }
+        
+        
+    } while (option != 0);
+}
+
+void freeMemory(Tarea **task, Tarea **task_completed,int quantity){
+    for (int i = 0; i < quantity; i++)
+    {
+        if (*(task+i) != NULL)
+        {
+            free((*(task+i))->Descripcion);
+        }
+
+        if (*(task_completed+i) != NULL)
+        {
+            free((*(task_completed+i))->Descripcion);
+        }
+        free(*(task+i));
+        free(*(task_completed+i))
+    }
+    free(task);
+    free(task_completed);
+}
+
+
+
+
+
+
+//printf("su tarea solicitada fue encontrada\n");
+        //    printf("La tarea se encuentra entre la lista de tareas pendientes\n");
+          //  printf("descripcion de la tarea:");
